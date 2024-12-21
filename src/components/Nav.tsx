8133b1841f09559cd091d377cbcc886f9566f211
Justin Miller <justinmiller617@gmail.com>
2024-12-21 12:05:19 -0600

    created Nav within Nav.tsx and included css properties

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,45 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav style={styles.nav}>
+      <ul style={styles.navList}>
+        <li>
+          <Link to="/" style={styles.link}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/saved-candidates" style={styles.link}>
+            Potential Candidates
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const styles = {
+  nav: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center", 
+    padding: "1rem",
+    color: "#ecf0f1",
+  },
+  navList: {
+    listStyleType: "none",
+    display: "flex",
+    gap: "1.5rem",
+    margin: 0,
+    padding: 0,
+  },
+  link: {
+    color: "#ecf0f1",
+    textDecoration: "none",
+    fontSize: "1.2rem",
+    fontWeight: "500",
+  },
 };
 
 export default Nav;
